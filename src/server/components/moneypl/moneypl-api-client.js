1391dab463a8f6bc3c444391ffbe7260185065f2
Grzegorz Wasiak <g.wasiak@gmail.com>
2015-09-10 21:27:47 +0200

    multiple requests to moneypl API due to constraints

--- src/server/components/moneypl/moneypl-api-client.js
+++ src/server/components/moneypl/moneypl-api-client.js
@@ -1,34 +1,64 @@
 import _ from "lodash";
 import request from "request";
 
-const API_URL_FUND_HISTORY = "http://www.money.pl/fundusze/archiwum/fundusze/";
+const API_FUND_HISTORY_URL = "http://www.money.pl/fundusze/archiwum/fundusze/";
+const API_FUND_HISTORY_PERIOD_DAYS_LIMIT = 50;
 
 class MoneyplApiClient {
     getFundHistoryResult(symbol, fromDate, toDate) {
         let from = fromDate.toISOString();
-        let fromAPIDate = this._encodeAPIDate(fromDate);
         let to = toDate.toISOString();
-        let toAPIDate = this._encodeAPIDate(toDate);
+        let meta = {symbol, from, to};
 
+        return this._requestFundHistory(symbol, fromDate, toDate)
+            .then((data) => ({meta, data}));
+    }
+
+    _requestFundHistory(symbol, fromDate, toDate) {
+        let partPeriods = this._evalFundHistoryPartPeriods(fromDate, toDate);
+
+        return Promise.all(_.map(partPeriods, (partPeriod) => this._requestFundHistoryPart(symbol, partPeriod.fromDate, partPeriod.toDate)))
+            .then((parts) => ({
+                symbol: parts[0].symbol,
+                records: _.reduce(parts, (data, part) => data.concat(part.records), [])
+            }));
+    }
+
+    _evalFundHistoryPartPeriods(fromDate, toDate) {
+        let partPeriods = [];
+        let periodDate;
+        let limitDate;
+
+        while (!limitDate || limitDate < toDate) {
+            periodDate = limitDate ? new Date(limitDate.getFullYear(), limitDate.getMonth(), limitDate.getDate() + 1) : fromDate;
+            limitDate = new Date(periodDate.getFullYear(), periodDate.getMonth(), periodDate.getDate() + API_FUND_HISTORY_PERIOD_DAYS_LIMIT);
+
+            partPeriods.push({
+                fromDate: periodDate,
+                toDate: limitDate < toDate ? limitDate : toDate
+            });
+        }
+
+        return partPeriods;
+    }
+
+    _requestFundHistoryPart(symbol, fromDate, toDate) {
         return new Promise((resolve, reject) => {
             request.post({
-                url: API_URL_FUND_HISTORY,
+                url: API_FUND_HISTORY_URL,
                 form: {
                     "symbol": symbol,
-                    "od": fromAPIDate,
-                    "do": toAPIDate,
+                    "od": this._encodeAPIDate(fromDate),
+                    "do": this._encodeAPIDate(toDate),
                     "period": 0,
                     "format": "csv"
                 }
-            }, (err, response, history) => {
+            }, (err, response, data) => {
                 if (err) {
                     return reject(err);
                 }
 
-                let meta = {symbol, from, to};
-                let data = this._sanitizeFundHistory(symbol, history);
-
-                resolve({meta, data});
+                resolve(this._sanitizeFundHistory(symbol, data));
             });
         });
     }
