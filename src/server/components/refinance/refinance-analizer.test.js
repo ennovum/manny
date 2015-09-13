732d84ff4133b5969ee8f7e018d2b1517048d9f2
Grzegorz Wasiak <g.wasiak@gmail.com>
2015-09-13 16:33:01 +0200

    refinance-analizer unit tests

--- src/server/components/refinance/refinance-analizer.test.js
+++ src/server/components/refinance/refinance-analizer.test.js
@@ -1,157 +1,313 @@
 /*global describe, it*/
 
+import moment from "moment";
 import chai from "chai";
 
 import RefinanceAnalizer from "./refinance-analizer.js";
+import {filterRecordsSpan, evalRatio} from "./refinance-analizer.js";
 
 let expect = chai.expect;
 
-describe("refinance-analizer", () => {
+describe("RefinanceAnalizer", () => {
     let analizer = new RefinanceAnalizer();
 
     let sample1 = {
-        symbol: "SYMBOL1",
-        records: null,
-        stat: {},
-        verdict: null
+        symbol: "SAMPLE1",
+        records: [
+            {date: new Date("2013-01-01"), value: 0},
+            {date: new Date("2014-01-01"), value: 200},
+            {date: new Date("2015-01-01"), value: 100},
+            {date: new Date("2015-03-01"), value: 95},
+            {date: new Date("2015-05-01"), value: 100},
+            {date: new Date("2015-07-01"), value: 105},
+            {date: new Date("2015-08-01"), value: 100},
+            {date: new Date("2015-08-11"), value: 101},
+            {date: new Date("2015-08-21"), value: 99},
+            {date: new Date("2015-09-01"), value: 100}
+        ],
+        date: new Date("2015-09-01")
     };
-    sample1.records = [
-        {date: new Date("2015-09-01"), value: 100},
-        {date: new Date("2015-09-02"), value: 75},
-        {date: new Date("2015-09-03"), value: 50},
-        {date: new Date("2015-09-04"), value: 75},
-        {date: new Date("2015-09-05"), value: 100},
-        {date: new Date("2015-09-06"), value: 150},
-        {date: new Date("2015-09-07"), value: 200},
-        {date: new Date("2015-09-08"), value: 150},
-        {date: new Date("2015-09-09"), value: 100},
-        {date: new Date("2015-09-10"), value: 150}
-    ];
-    sample1.stat.symbol = sample1.symbol;
-    sample1.stat.records = sample1.records;
-    sample1.stat.keyRecords = {
-        oldest: sample1.records[0],
-        newest: sample1.records[9],
-        lowest: sample1.records[2],
-        highest: sample1.records[6]
-    };
-    sample1.stat.keyValues = {
-        oldest: sample1.records[0].value,
-        newest: sample1.records[9].value,
-        lowest: sample1.records[2].value,
-        highest: sample1.records[6].value
-    };
-    sample1.stat.ratios = {
-        period: 0.50,
-        peakGrowth: 2.00,
-        peakDrop: -0.25
-    };
-    sample1.stat.alerts = {
-        periodGrowth: true,
-        periodDrop: false,
-        peakGrowth: true,
-        peakDrop: true
-    };
-    sample1.verdict = "FALL";
 
-    let sample2 = {
-        symbol: "SYMBOL2",
-        records: null,
-        stat: {},
-        verdict: null
-    };
-    sample2.records = [
+    sample1.recentRecords = [
+        sample1.records[6],
+        sample1.records[7],
+        sample1.records[8],
+        sample1.records[9]
+    ];
+
+    sample1.seasonRecords = [
+        sample1.records[2],
+        sample1.records[3],
         sample1.records[4],
         sample1.records[5],
-        sample1.records[6]
+        sample1.records[6],
+        sample1.records[7],
+        sample1.records[8],
+        sample1.records[9]
     ];
-    sample2.stat.symbol = sample2.symbol;
-    sample2.stat.records = sample2.records;
-    sample2.stat.keyRecords = {
-        oldest: sample1.records[4],
-        newest: sample1.records[6],
-        lowest: sample1.records[4],
-        highest: sample1.records[6]
+
+    sample1.recentKeyRecords = {
+        oldest: sample1.records[6],
+        newest: sample1.records[9]
     };
-    sample2.stat.keyValues = {
-        oldest: sample1.records[4].value,
-        newest: sample1.records[6].value,
-        lowest: sample1.records[4].value,
-        highest: sample1.records[6].value
+
+    sample1.seasonKeyRecords = {
+        oldest: sample1.records[2],
+        newest: sample1.records[9],
+        lowest: sample1.records[3],
+        highest: sample1.records[5]
     };
-    sample2.stat.ratios = {
-        period: 1.00,
-        peakGrowth: 1.00,
-        peakDrop: 0.00
+
+    sample1.recentKeyValues = {
+        oldest: sample1.records[6].value, // 100
+        newest: sample1.records[9].value // 100
     };
-    sample2.stat.alerts = {
-        periodGrowth: true,
-        periodDrop: false,
-        peakGrowth: true,
-        peakDrop: false
+
+    sample1.seasonKeyValues = {
+        oldest: sample1.records[2].value, // 100
+        newest: sample1.records[9].value, // 100
+        lowest: sample1.records[3].value, // 95
+        highest: sample1.records[5].value // 105
     };
-    sample2.verdict = "RISE";
 
-    it("should evaluate stat key records", () => {
-        let keyRecords;
+    sample1.recentRatios = {
+        global: 0
+    };
 
-        keyRecords = analizer._evalStatKeyRecords(sample1.records);
-        expect(keyRecords).to.deep.equal(sample1.stat.keyRecords);
+    sample1.seasonRatios = {
+        peakRise: 0.0526,
+        peakFall: -0.0476
+    };
 
-        keyRecords = analizer._evalStatKeyRecords(sample2.records);
-        expect(keyRecords).to.deep.equal(sample2.stat.keyRecords);
+    sample1.recentAlerts = {
+        globalRise: false,
+        globalFall: false
+    };
+
+    sample1.seasonAlerts = {
+        peakRise: false,
+        peakFall: false
+    };
+
+    sample1.recentStat = {
+        records: sample1.recentRecords,
+        keyRecords: sample1.recentKeyRecords,
+        keyValues: sample1.recentKeyValues,
+        ratios: sample1.recentRatios,
+        alerts: sample1.recentAlerts
+    };
+
+    sample1.seasonStat = {
+        records: sample1.seasonRecords,
+        keyRecords: sample1.seasonKeyRecords,
+        keyValues: sample1.seasonKeyValues,
+        ratios: sample1.seasonRatios,
+        alerts: sample1.seasonAlerts
+    };
+
+    sample1.statset = {
+        symbol: sample1.symbol,
+        recent: sample1.recentStat,
+        season: sample1.seasonStat
+    };
+
+    sample1.statsets = [
+        sample1.statset
+    ];
+
+    sample1.verdict = "STILL";
+
+    let sample2 = {
+        statsets: [
+            {
+                recent: {
+                    alerts: {
+                        globalRise: false,
+                        globalFall: true
+                    }
+                },
+                season: {
+                    alerts: {
+                        peakRise: false,
+                        peakFall: false
+                    }
+                }
+            },
+            {
+                recent: {
+                    alerts: {
+                        globalRise: false,
+                        globalFall: false
+                    }
+                },
+                season: {
+                    alerts: {
+                        peakRise: true,
+                        peakFall: false
+                    }
+                }
+            }
+        ]
+    };
+
+    sample2.verdict = "FALL";
+
+    it("should get statset", () => {
+        let statset = analizer._getRecordsStatset(sample1.symbol, sample1.records, sample1.date);
+        expect(statset).to.deep.equal(sample1.statset);
     });
 
-    it("should evaluate stat key values", () => {
-        let keyValues;
-
-        keyValues = analizer._evalStatKeyValues(sample1.stat.keyRecords);
-        expect(keyValues).to.deep.equal(sample1.stat.keyValues);
-
-        keyValues = analizer._evalStatKeyValues(sample2.stat.keyRecords);
-        expect(keyValues).to.deep.equal(sample2.stat.keyValues);
+    it("should filter recent records", () => {
+        let recentRecords = analizer._filterRecentRecords(sample1.records, sample1.date);
+        expect(recentRecords).to.deep.equal(sample1.recentRecords);
     });
 
-    it("should evaluate stat ratios", () => {
-        let ratios;
-
-        ratios = analizer._evalStatRatios(sample1.stat.keyValues);
-        expect(ratios).to.deep.equal(sample1.stat.ratios);
-
-        ratios = analizer._evalStatRatios(sample2.stat.keyValues);
-        expect(ratios).to.deep.equal(sample2.stat.ratios);
+    it("should filter season records", () => {
+        let seasonRecords = analizer._filterSeasonRecords(sample1.records, sample1.date);
+        expect(seasonRecords).to.deep.equal(sample1.seasonRecords);
     });
 
-    it("should evaluate stat alerts", () => {
-        let alerts;
-
-        alerts = analizer._evalStatAlerts(sample1.stat.ratios);
-        expect(alerts).to.deep.equal(sample1.stat.alerts);
-
-        alerts = analizer._evalStatAlerts(sample2.stat.ratios);
-        expect(alerts).to.deep.equal(sample2.stat.alerts);
+    it("should get recent stat", () => {
+        let recentStat = analizer._getRecentRecordsStat(sample1.recentRecords);
+        expect(recentStat).to.deep.equal(sample1.recentStat);
     });
 
-    it("should evaluate stat", () => {
-        let stat;
-
-        stat = analizer._getRecordsStat(sample1.symbol, sample1.records);
-        expect(stat).to.deep.equal(sample1.stat);
-
-        stat = analizer._getRecordsStat(sample2.symbol, sample2.records);
-        expect(stat).to.deep.equal(sample2.stat);
+    it("should get season stat", () => {
+        let seasonStat = analizer._getSeasonRecordsStat(sample1.seasonRecords);
+        expect(seasonStat).to.deep.equal(sample1.seasonStat);
     });
 
-    it("should evaluate verdict", () => {
+    it("should eval recent key records", () => {
+        let recentKeyRecords = analizer._evalRecentKeyRecords(sample1.recentRecords);
+        expect(recentKeyRecords).to.deep.equal(sample1.recentKeyRecords);
+    });
+
+    it("should eval season key records", () => {
+        let seasonKeyRecords = analizer._evalSeasonKeyRecords(sample1.seasonRecords);
+        expect(seasonKeyRecords).to.deep.equal(sample1.seasonKeyRecords);
+    });
+
+    it("should eval recent key values", () => {
+        let recentKeyValues = analizer._evalRecentKeyValues(sample1.recentKeyRecords);
+        expect(recentKeyValues).to.deep.equal(sample1.recentKeyValues);
+    });
+
+    it("should eval season key values", () => {
+        let seasonKeyValues = analizer._evalSeasonKeyValues(sample1.seasonKeyRecords);
+        expect(seasonKeyValues).to.deep.equal(sample1.seasonKeyValues);
+    });
+
+    it("should eval recent ratios", () => {
+        let recentRatios = analizer._evalRecentRatios(sample1.recentKeyValues);
+        expect(recentRatios).to.deep.equal(sample1.recentRatios);
+    });
+
+    it("should eval season ratios", () => {
+        let seasonRatios = analizer._evalSeasonRatios(sample1.seasonKeyValues);
+        expect(seasonRatios).to.deep.equal(sample1.seasonRatios);
+    });
+
+    it("should eval recent alerts", () => {
+        let recentAlerts = analizer._evalRecentAlerts(sample1.recentRatios);
+        expect(recentAlerts).to.deep.equal(sample1.recentAlerts);
+    });
+
+    it("should eval season alerts", () => {
+        let seasonAlerts = analizer._evalSeasonAlerts(sample1.seasonRatios);
+        expect(seasonAlerts).to.deep.equal(sample1.seasonAlerts);
+    });
+
+    it("should get statsets verdict", () => {
         let verdict;
 
-        verdict = analizer._getStatsVerdict([sample1.stat]);
-        expect(verdict).to.equal(sample1.verdict);
+        verdict = analizer._getStatsetsVerdict(sample1.statsets);
+        expect(verdict).to.deep.equal(sample1.verdict);
 
-        verdict = analizer._getStatsVerdict([sample2.stat]);
-        expect(verdict).to.equal(sample2.verdict);
+        verdict = analizer._getStatsetsVerdict(sample2.statsets);
+        expect(verdict).to.deep.equal(sample2.verdict);
+    });
+});
 
-        verdict = analizer._getStatsVerdict([sample1.stat, sample2.stat]);
-        expect(verdict).to.equal(sample1.verdict);
+describe("filterRecordsSpan", () => {
+    let sample1 = {
+        records: [
+            {date: new Date("2013-01-01"), value: 0},
+            {date: new Date("2014-01-01"), value: 200},
+            {date: new Date("2015-01-01"), value: 100},
+            {date: new Date("2015-03-01"), value: 95},
+            {date: new Date("2015-05-01"), value: 100},
+            {date: new Date("2015-07-01"), value: 105},
+            {date: new Date("2015-08-01"), value: 100},
+            {date: new Date("2015-08-11"), value: 101},
+            {date: new Date("2015-08-21"), value: 99},
+            {date: new Date("2015-09-01"), value: 100}
+        ],
+        endDate: new Date("2015-09-01"),
+        duration: moment.duration({months: 1})
+    };
+
+    sample1.filteredRecords = [
+        sample1.records[6],
+        sample1.records[7],
+        sample1.records[8],
+        sample1.records[9]
+    ];
+
+    let sample2 = {
+        records: sample1.records,
+        endDate: new Date("2015-07-01"),
+        duration: moment.duration({years: 1})
+    };
+
+    sample2.filteredRecords = [
+        sample1.records[2],
+        sample1.records[3],
+        sample1.records[4],
+        sample1.records[5]
+    ];
+
+    it("should filter records span", () => {
+        let filteredRecords;
+
+        filteredRecords = filterRecordsSpan(sample1.records, sample1.endDate, sample1.duration);
+        expect(filteredRecords).to.deep.equal(sample1.filteredRecords);
+
+        filteredRecords = filterRecordsSpan(sample2.records, sample2.endDate, sample2.duration);
+        expect(filteredRecords).to.deep.equal(sample2.filteredRecords);
+    });
+});
+describe("evalRatio", () => {
+    let sample1 = {
+        value: 100,
+        relativeValue: 100
+    };
+
+    sample1.ratio = 0;
+
+    let sample2 = {
+        value: 100,
+        relativeValue: 50
+    };
+
+    sample2.ratio = 1;
+
+    let sample3 = {
+        value: 100,
+        relativeValue: 200
+    };
+
+    sample3.ratio = -0.5;
+
+    it("should eval ratio", () => {
+        let ratio;
+
+        ratio = evalRatio(sample1.value, sample1.relativeValue);
+        expect(ratio).to.equal(sample1.ratio);
+
+        ratio = evalRatio(sample2.value, sample2.relativeValue);
+        expect(ratio).to.equal(sample2.ratio);
+
+        ratio = evalRatio(sample3.value, sample3.relativeValue);
+        expect(ratio).to.equal(sample3.ratio);
     });
 });
